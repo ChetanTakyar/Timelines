9ec70fa31d1e651a964b9952bed2461920ccf7d1
Chetan Takyar <takyarchetan@gmail.com>
2020-09-13 17:53:11 +1200

    Removed unused variable

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 function DarkModeToggle(){
-	const toggleButton = document.querySelector("dark-mode-toggle");
 	const theme = document.querySelector("#theme-type");
-
 	if(theme.getAttribute("href") == "lightmode.css"){
 		theme.href = "darkmode.css";
 	} else{
