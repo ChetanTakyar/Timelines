eb0f090be2a363753b59ac9599182ad03479a5de
Chetan Takyar <takyarchetan@gmail.com>
2022-08-14 08:35:25 +1000

    Added variable to save theme state in JS file.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+var themeLight;
 function DarkModeToggle() {
 	const theme = document.querySelector("#theme-type");
 	if (theme.getAttribute("href") == "lightmode.css") {
 		theme.href = "darkmode.css";
+		themeLight = false;
 	} else {
 		theme.href = "lightmode.css";
+		themeLight = true;
 	}
 }
