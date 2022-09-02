3c87d4bb1b26b42c5c469820098399ca231e5016
Chetan Takyar <takyarchetan@gmail.com>
2022-09-02 23:31:03 +1000

    Added iPadOS activeTab function, added onclick to html

--- scripts/staticNavBarColourOnClick.js
+++ scripts/staticNavBarColourOnClick.js
@@ -2,35 +2,52 @@ const watchOSPage = document.getElementsByID("watchOSPage");
 const macOSPage = document.getElementById("macOSPage");
 const tvOSPage = document.getElementById("tvOSPage");
 const iOSPage =  document.getElementsByID("iOSPage");
-
-
+const iPadOSPage = document.getElementById("iPadOSPage");
 
 
 function changeWatchOSColourOnClick(){
-    watchOSPage.classlist.add("activeTab");
+
+    watchOSPage.classList.add("activeTab");
     macOSPage.classList.remove("activeTab");
     tvOSPage.classList.remove("activeTab");
     iOSPage.classList.remove("activeTab");
+    iPadOSPage.classList.remove("activeTab");
+
 }
 
 function changeMacOSColourOnClick(){
+
     macOSPage.classList.add("activeTab");
     watchOSPage.classList.remove("activeTab");
     tvOSPage.classList.remove("activeTab");
     iOSPage.classList.remove("activeTab");
+    iPadOSPage.classList.remove("activeTab");
 
 }
 
 function changetvOSColourOnClick(){
+
     tvOSPage.classList.add("activeTab");
     watchOSPage.classList.remove("activeTab");
     iOSPage.classList.remove("activeTab");
     macOSPage.classList.remove("activeTab");
+    iPadOSPage.classList.remove("activeTab");
+
 }
 
 function changeiOSColourOnClick(){
+
    iOSPage.classList.add("activeTab");
    watchOSPage.classList.remove("activeTab");
    macOSPage.classList.remove("activeTab");
    tvOSPage.classList.remove("activeTab");
+   iPadOSPage.classList.remove("activeTab");
 }
+function changeiPadOSColourOnClick(){
+    
+    iPadOSPage.classList.add("activeTab")
+    iOSPage.classList.remove("activeTab");
+    watchOSPage.classList.remove("activeTab");
+    macOSPage.classList.remove("activeTab");
+    tvOSPage.classList.remove("activeTab");
+ }
